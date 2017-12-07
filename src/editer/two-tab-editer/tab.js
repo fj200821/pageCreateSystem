6cefe797b4b66e117b3325502d7524a2ea281b42
31916088 <258220040@qq.com>
2017-12-07 20:08:06 +0800

    fix

--- src/editer/two-tab-editer/tab.js
+++ src/editer/two-tab-editer/tab.js
@@ -1,30 +1,31 @@
 import React, {Component} from 'react';
-import {Form, Button, Input, InputNumber, Radio, Checkbox, Select,message} from 'antd';
+import {Form, Button, Input, InputNumber, Radio, Checkbox, Select, message} from 'antd';
 import Util from '../../compoents/util/util';
 import Upload from '../../compoents/upload';
+
 const FormItem = Form.Item;
 const RadioGroup = Radio.Group;
 const Option = Select.Option;
-const types = {
-    6:{
-        name:"指定计划",
-        placeholder:"请输入计划id",
-        type:"planId"
+const actions = {
+    6: {
+        name: "指定计划",
+        placeholder: "请输入计划id",
+        type: "planId"
     },
-    4:{
-        name:"指定类目",
-        placeholder:"请输入类目id",
-        type:"industryId"
+    4: {
+        name: "指定类目",
+        placeholder: "请输入类目id",
+        type: "industryId"
     },
-    3:{
-        name:"游戏互动",
-        placeholder:"请输入游戏id",
-        type:"lotteryId"
+    3: {
+        name: "游戏互动",
+        placeholder: "请输入游戏id",
+        type: "lotteryId"
     },
-    5:{
-        name:"链接",
-        placeholder:"请输入链接地址",
-        type:"skipUrl"
+    5: {
+        name: "链接",
+        placeholder: "请输入链接地址",
+        type: "skipUrl"
     },
 };
 
@@ -32,78 +33,48 @@ class Tab extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            item: {
-                type: 6,
-                noCharging: false,
-                picUrl: '//oss.ltcdn.cc/cow/2017/12/07/231w_221h_1322A1512613862_origin.png'
-            }
+            item: props.item
         };
-        if (props.item) {
-            this.state.item = props.item;
-        }
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            item: nextProps.item || {}
-        });
-        console.log(nextProps.item)
+        this.change(props.item.type);
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        let item = this.state.item;
-        if(!item[types[item.type].type]){
-            message.error(types[item.type].placeholder);
-            return;
-        }
-        this.props.callback(Object.assign({},this.state.item));
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (err) {
+                console.log('Received values of form: ', values);
+                return;
+            }
+            values.picUrl = this.state.item.picUrl;
+            this.props.callback(values);
+        });
     };
 
     updateCallback = (res) => {
-        let item = this.state.item;
+        let item = Object.assign({},this.state.item);
         item.picUrl = res.data.url;
     };
 
-    changeNoCharging = (e) => {
-        this.change(e.target.checked,'noCharging');
+
+    change = (value) => {
+        this.renderType = () => {
+            const {getFieldDecorator} = this.props.form;
+            let type = actions[value].type;
+            return <FormItem
+                {...Util.tailFormItemLayout}>
+                {getFieldDecorator(type, {
+                    initialValue: this.state.item[type],
+                    rules: [{required: true}],
+                })(
+                    <Input placeholder={actions[value].placeholder}/>
+                )}
+            </FormItem>
+        }
     };
 
-
-    changeEditerTabType=(value,name)=>{
-        let item = JSON.parse(JSON.stringify(this.state.item));
-        Object.keys(types).forEach((key)=>{
-            let type = types[key].type;
-            delete item[type];
-        });
-        this.setState({
-            item: item
-        },()=>{
-            this.change(value,name);
-        });
-    };
-
-    change = (value,name) => {
-        let item = JSON.parse(JSON.stringify(this.state.item));
-        item[name] = value;
-
-        this.setState({
-            item: item
-        });
-    };
-
-    renderType(){
-        let showType = this.state.item.type;
-        return <div>
-            {Object.keys(types).map((key,index)=>{
-                let type = types[key].type;
-                return <Input value={this.state.item[type]} style={{"display":showType.toString()===key?'block':'none'}} placeholder={types[key].placeholder} key={index} onChange={(e)=>{this.changeEditerTabType(e.target.value,type)}}/>
-            })}
-        </div>
-    }
 
     render() {
-
+        const {getFieldDecorator} = this.props.form;
         return (
             <div>
                 <div style={{"marginBottom": "20px"}}>
@@ -114,22 +85,32 @@ class Tab extends Component {
                         label="跳转类型"
                         {...Util.formItemLayout}
                     >
-                        <Select value={types[this.state.item.type].name} onChange={(value,e)=>{this.change(value,'type');}}>
-                            {Object.keys(types).map((key) => {
-                                return <Option value={key} key={key}>{types[key].name}</Option>;
-                            })}
-                        </Select>
+                        {getFieldDecorator('type', {
+                            initialValue: actions[this.state.item.type].name,
+                            rules: [{required: true}],
+                            onChange: (value) => {
+                                this.change(value);
+                            }
+                        })(
+                            <Select>
+                                {Object.keys(actions).map((key) => {
+                                    return <Option value={key} key={key}>{actions[key].name}</Option>;
+                                })}
+                            </Select>
+                        )}
                     </FormItem>
 
-                    <FormItem
-                        {...Util.tailFormItemLayout}>
-                        {this.renderType()}
-                    </FormItem>
+                    {this.renderType()}
 
                     <FormItem
                         label="是否计费"
                         {...Util.formItemLayout}>
-                        <Checkbox onChange={this.changeNoCharging}>不计费</Checkbox>
+                        {getFieldDecorator('noCharging', {
+                            initialValue:this.state.item.noCharging,
+                            valuePropName:"checked"
+                        })(
+                            <Checkbox>不计费</Checkbox>
+                        )}
                     </FormItem>
                     <FormItem {...Util.tailFormItemLayout}>
                         <Button htmlType="submit" type="primary">确定</Button>
@@ -140,4 +121,22 @@ class Tab extends Component {
     }
 }
 
-export default Tab
+
+class Middle extends Tab {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            item: nextProps.item || {}
+        });
+    }
+
+    render() {
+        let Temp = Form.create()(Tab);
+        return <Temp item={this.state.item} callback={this.props.callback}/>
+    }
+}
+
+export default Middle
